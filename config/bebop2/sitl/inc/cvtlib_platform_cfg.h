147acf4dd4f9517274b4093289ea9223afc81a5a
Mathew Benson <mbenson@windhoverlabs.com>
2019-11-26 18:33:29 -0600

    Initial checkin of CVT and CVTLib.

---
+++ config/bebop2/sitl/inc/cvtlib_platform_cfg.h
@@ -0,0 +1,20 @@
+    
+#ifndef CVT_PLATFORM_CFG_H
+#define CVT_PLATFORM_CFG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CVT_PLATFORM_CFG_H */
+
+/************************/
+/*  End of File Comment */
+/************************/
+    
